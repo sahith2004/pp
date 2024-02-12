737375d87a0ff817502c386a685e8b89ab552830
shanmukha sahith <92036395+sahith2004@users.noreply.github.com>
2024-02-12 19:31:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -40,8 +40,7 @@ def get_vector_store(text_chunks):
 def get_conversational_chain():
 
     prompt_template = """
-    Answer the question as detailed as possible from the provided context, make sure to provide all the details, if the answer is not in
-    provided context just say, "answer is not available in the context", don't provide the wrong answer\n\n
+    You are a Q&A assistant. Your goal is to answer questions as accurately as possible based on the instructions and context provided.
     Context:\n {context}?\n
     Question: \n{question}\n
 
